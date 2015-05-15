4ff0dac71ec0654fe296ab5113e71644f3a2a9cf
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-05-15 21:39:27 +0200

    added api test message

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/api/test', function(req, res) {
+    res.json({
+        status_code: 200,
+        status: 'OK',
+        content: {
+            type: 'JSON',
+            message: 'Working!'
+        }
+    })
+});
+
 app.listen(3000, function() {
 	console.log("Server ready. Listening on port 3000");
 });
